48bca52f73946ea9cf79f7b347bcf858dafe1ede
Hilmi Yahya <hilmiyahya199@gmail.com>
2024-05-09 07:35:45 +0700

    update

--- src/pages/PrivatePage/Dashboard.jsx
+++ src/pages/PrivatePage/Dashboard.jsx
@@ -1,13 +1,44 @@
-import Hero from "../../components/Hero/Hero"
-import Movies from "../../components/Movies/Movies"
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+// import Hero from "../../components/Hero/Hero";
 
 function Dashboard() {
   return (
     <>
-       <Hero/>
-       <Movies title="Latest Movie"/>
+      {/* <Hero /> */}
+      <div className="container mx-auto py-10 px-4 mt-10 mb-10 shadow-lg bg-slate-100 rounded-lg">
+        <Text className="font-semibold text-2xl text-center mb-6">
+          Manage your content here
+        </Text>
+        <Flex justifyContent="center">
+          <div>
+            <Link to="/manage/create">
+              <Button colorScheme="blue">click here</Button>
+            </Link>
+          </div>
+        </Flex>
+      </div>
+      <div className="container mx-auto py-10 px-4 mt-10 mb-10 shadow-lg bg-slate-100 rounded-lg">
+        <Text className="font-semibold text-2xl text-center mb-6">
+          Upload your video or image files locally here
+        </Text>
+        <Flex justifyContent="center" gap={8}>
+          <div>
+            <Text className="text-lg mb-2 text-center">Upload Video</Text>
+            <Link to="/upload-video">
+              <Button colorScheme="blue">Upload Video</Button>
+            </Link>
+          </div>
+          <div>
+            <Text className="text-lg mb-2 text-center">Upload Image</Text>
+            <Link to="/upload-image">
+              <Button colorScheme="blue">Upload Image</Button>
+            </Link>
+          </div>
+        </Flex>
+      </div>
     </>
-  )
+  );
 }
 
 export default Dashboard;
